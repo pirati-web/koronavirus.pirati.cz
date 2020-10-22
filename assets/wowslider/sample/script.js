07fab5ee8ed7d12d7024104d7d07c8ab440d131b
Jan Suchánek <jan.suchanek@gmail.com>
2020-10-22 15:46:23 +0200

    Update script.js

--- assets/wowslider/sample/script.js
+++ assets/wowslider/sample/script.js
@@ -1,3 +1,3 @@
 $( document ).ready(function() {
-    jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:50*100,delay:20*100,width:580,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
+    jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:50,delay:20,width:280,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
 });
